cfe3fbef4a04353abf7580b7999605fb695fb48e
Bruna Silva <brunacsilva11@outlook.com>
2023-09-28 23:46:08 -0300

    Correções e adequações código controller e service

--- back-end/src/controllers/controller.js
+++ back-end/src/controllers/controller.js
@@ -1,63 +1,70 @@
-const battleService = require("../services/battleService")
+const battleService = require("../services/battleService");
 
 module.exports = {
-    async newBattle(req,res){        
+  async newBattle(req, res) {
+    try {
+      let combatente01 = req.body.pokemon01;
+      let combatente02 = req.body.pokemon02;
 
-        try
-        {       
-            const combatente01 = req.body.pokemon01
-            const combatente02 = req.body.pokemon02
-            
-            combatente01 = battleService.getAttributesOfPokemon(combatente01)
-            combatente02 = battleService.getAttributesOfPokemon(combatente02)
+      combatente01 = battleService.getAttributesOfPokemon(combatente01);
+      combatente02 = battleService.getAttributesOfPokemon(combatente02);
 
-            var result = battleService.calculationBattle(combatente01,combatente02)
+      const result = battleService.calculationBattle(combatente01, combatente02);
 
-            console.log(combatente01,combatente02,result)
-            res.json({message:result, pokemons:[combatente01,combatente02]})
-
-        }
-        catch (e) {
-            return res.status(400).json({
-                sucess: false,
-                error: e.response ? e.response.data : "There was an error on the server, the battle need wait"
-            })
-        }
-    },
-    async newPokemon(req,res){
-        try{
-            // Extrair os dados da requisição
-            const { nome, nivel, hp, ataque, defesa, velocidade } = req.body;
-                    
-            // Criar uma nova instância de Pokemon
-            const novoPokemon = new Pokemon(
-              nome,
-              nivel,
-              hp.BS,
-              hp.IV,
-              hp.EV,
-              ataque.BS,
-              ataque.IV,
-              ataque.EV,
-              defesa.BS,
-              defesa.IV,
-              defesa.EV,
-              velocidade.BS,
-              velocidade.IV,
-              velocidade.EV
-            );
-            
-            // Você pode fazer mais ações aqui, como salvar o novo Pokémon em um banco de dados
-            
-            // Responder com o novo Pokémon criado
-            res.json({ message: 'Novo Pokémon criado com sucesso!', pokemon: novoPokemon });
-        }
-        catch(e){
-            return res.status(400).json({
-                sucess: false,
-                error: e.response ? e.response.data : "Error can't create Pokémon"
-            })
-        }
-
+      console.log(combatente01, combatente02, result);
+      res.json({ message: result, pokemons: [combatente01, combatente02] });
+    } catch (e) {
+      return res.status(400).json({
+        success: false,
+        error: e.response ? e.response.data : "Houve um erro no servidor, a batalha precisa esperar.",
+      });
     }
-}
+  },
+
+  async newPokemon(req, res) {
+    try {
+      // Extrair os dados da requisição
+      const { nome, nivel, hp, ataque, defesa, velocidade } = req.body;
+
+      // Criar uma nova instância de Pokemon
+      const novoPokemon = {
+        nome,
+        nivel,
+        hp: {
+          BS: hp.BS,
+          IV: hp.IV,
+          EV: hp.EV,
+          Value: null,
+        },
+        ataque: {
+          BS: ataque.BS,
+          IV: ataque.IV,
+          EV: ataque.EV,
+          Value: null,
+        },
+        defesa: {
+          BS: defesa.BS,
+          IV: defesa.IV,
+          EV: defesa.EV,
+          Value: null,
+        },
+        velocidade: {
+          BS: velocidade.BS,
+          IV: velocidade.IV,
+          EV: velocidade.EV,
+          Value: null,
+        },
+      };
+
+      // Você pode fazer mais ações aqui, como salvar o novo Pokémon em um banco de dados
+
+      // Responder com o novo Pokémon criado
+      res.json({ message: "Novo Pokémon criado com sucesso!", pokemon: novoPokemon });
+    } catch (e) {
+      return res.status(400).json({
+        success: false,
+        error: e.response ? e.response.data : "Erro ao criar Pokémon.",
+      });
+    }
+  },
+};
--- back-end/src/services/battleService.js
+++ back-end/src/services/battleService.js
@@ -1,45 +1,43 @@
 module.exports = {
-    
-    calculationAttributes(itIsHP,IV,BS,EV,L){
-        //Método que faz o cálculo da fórmula dos atributos
-        return ((((IV + BS + (Math.SQRT2(EV)/8)) + (itIsHP? 50 : 0)) * L)/50) + (itIsHP? 10 : 5)
+    calculationAttributes(isHP, IV, BS, EV, L) {
+      // Método que faz o cálculo da fórmula dos atributos
+      return (((IV + BS + (Math.sqrt(2) * EV) / 8) + (isHP ? 50 : 0)) * L) / 50 + (isHP ? 10 : 5);
     },
-
-    getAttributesOfPokemon(pokemon){
-        pokemon.ataque.Value = this.calculationAttributes(false,pokemon.ataque.IV, pokemon.ataque.BS, pokemon.ataque.EV,pokemon.nivel)
-        pokemon.defesa.Value = this.calculationAttributes(false,pokemon.defesa.IV, pokemon.defesa.BS, pokemon.defesa.EV,pokemon.nivel)
-        pokemon.velocidade.Value = this.calculationAttributes(false,pokemon.velocidade.IV, pokemon.velocidade.BS, pokemon.velocidade.EV,pokemon.nivel)
-        pokemon.hp.Value = this.calculationAttributes(true,pokemon.hp.IV, pokemon.hp.BS, pokemon.hp.EV,pokemon.nivel)
-
-        return pokemon
+  
+    getAttributesOfPokemon(pokemon) {
+      pokemon.ataque.Value = this.calculationAttributes(false, pokemon.ataque.IV, pokemon.ataque.BS, pokemon.ataque.EV, pokemon.nivel);
+      pokemon.defesa.Value = this.calculationAttributes(false, pokemon.defesa.IV, pokemon.defesa.BS, pokemon.defesa.EV, pokemon.nivel);
+      pokemon.velocidade.Value = this.calculationAttributes(false, pokemon.velocidade.IV, pokemon.velocidade.BS, pokemon.velocidade.EV, pokemon.nivel);
+      pokemon.hp.Value = this.calculationAttributes(true, pokemon.hp.IV, pokemon.hp.BS, pokemon.hp.EV, pokemon.nivel);
+  
+      return pokemon;
     },
-
-    calculationDamage(ataqueOponente,defesaPropria){
-        return ataqueOponente-defesaPropria
+  
+    calculationDamage(ataqueOponente, defesaPropria) {
+      return ataqueOponente - defesaPropria;
     },
-
-    defineWinner(damageOf01,damageOf02,HP01,HP02){
-        roundFor01Loses = Math.ceil(HP01/damageOf02)
-        roundFor02Loses = Math.ceil(HP02/damageOf01)
-        return [roundFor01Loses,roundFor02Loses]
+  
+    defineQtdRounds(damageOf01, damageOf02, HP01, HP02) {
+      const roundFor01Loses = Math.ceil(HP01 / damageOf02);
+      const roundFor02Loses = Math.ceil(HP02 / damageOf01);
+      return [roundFor01Loses, roundFor02Loses];
     },
-
-    calculationBattle(pokemon01,pokemon02){
-
-        var vencedor = new Pokemon()
-        var damageOf01 = this.calculationDamage(pokemon02.ataque.Value,pokemon01.defesa.Value)
-        var damageOf02 = this.calculationDamage(pokemon01.ataque.Value,pokemon02.defesa.Value)
-        const {roundFor01Loses,roundFor02Loses} = this.defineWinner(damageOf01,damageOf02,pokemon01.hp.Value,pokemon02.hp.Value)
-        if(roundFor01Loses == roundFor02Loses)
-        {
-            vencedor = pokemon01.velocidade.Value > pokemon02.velocidade.Value? pokemon01 : pokemon01.velocidade.Value == pokemon02.velocidade.Value? null : pokemon02
-        }
-        else
-        {
-            vencedor = roundFor01Loses > roundFor02Loses? pokemon01 : pokemon02
-        }
-
-        return vencedor == null? `A batalha empata e dura ${roundFor01Loses} turnos` 
-                                : `A batalha acaba em ${vencedor.nome==pokemon01.nome? roundFor02Loses : roundFor01Loses} turnos. Vencedor: ${vencedor.nome}`
-    }
-}
+  
+    calculationBattle(pokemon01, pokemon02) {
+      let vencedor = null;
+      const damageOf01 = this.calculationDamage(pokemon02.ataque.Value, pokemon01.defesa.Value);
+      const damageOf02 = this.calculationDamage(pokemon01.ataque.Value, pokemon02.defesa.Value);
+      const [roundFor01Loses, roundFor02Loses] = this.defineQtdRounds(damageOf01, damageOf02, pokemon01.hp.Value, pokemon02.hp.Value);
+  
+      if (roundFor01Loses === roundFor02Loses) {
+        vencedor = (pokemon01.velocidade.Value > pokemon02.velocidade.Value) ? pokemon01 :
+          (pokemon01.velocidade.Value === pokemon02.velocidade.Value) ? null : pokemon02;
+      } else {
+        vencedor = (roundFor01Loses > roundFor02Loses) ? pokemon01 : pokemon02;
+      }
+  
+      return vencedor === null ? `A batalha empata e dura ${roundFor01Loses} turnos` :
+        `A batalha acaba em ${(vencedor === pokemon01) ? roundFor02Loses : roundFor01Loses} turnos. Vencedor: ${vencedor.nome}`;
+    },
+  };
+  
